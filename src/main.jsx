8295cfcf982bbc67c621b718d2ef31ef977f6b55
ARSHATH AHAMED <sparkarshath23@gmail.com>
2024-07-04 20:37:36 +0530

    Updated all

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
-<<<<<<< HEAD
-import ReactGA from "react-ga4";
-import { AllPostsProvider } from "./contexts/AllPostsProvider"; // Adjust the path as necessary
-
-ReactGA.initialize("G-QK2T5N8MC9");
-
-// Send pageview with a custom path
-// ReactGA.send({ hitType: "pageview", page: "/logiin", title: "suiii" });
-=======
 import { AllPostsProvider } from "./contexts/AllPostsProvider";
->>>>>>> 64e24aa (Updated)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
