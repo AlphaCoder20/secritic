c517742b633801aca984be29809b0766ad8c6ab5
ARSHATH AHAMED <sparkarshath23@gmail.com>
2024-07-04 21:02:12 +0530

    env added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -101,7 +101,7 @@ export default function Search() {
         return false;
       });
 
-      // Check for match in section title if no match in posts
+      // Checking for match in section title if no match in posts
       if (!isMatch) {
         const sectionTitleWords = filterStopWords(
           group.sectionTitle.toLowerCase().split(" ")
